a8fd7acededa6385947aac80f13da04f928fecf0
Daniel Maina <daniel.maina@frontdoorhome.com>
2024-11-15 18:25:39 +0300

    fix: render home and triage web pages

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Root from './Root.tsx'
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const queryClient = new QueryClient();
-const router = createBrowserRouter({
+const router = createBrowserRouter([{
     path: "/",
     element: <Root />,
     children: [
@@ -19,7 +19,7 @@ const router = createBrowserRouter({
             element: <h1>Manage Triage Steps</h1>,
         }
     ]
-})
+}])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
