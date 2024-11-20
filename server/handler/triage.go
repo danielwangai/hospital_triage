31b0ff5ae003dacadce5d5b9ad005113fea9cb2a
Daniel Maina <daniel.maina@frontdoorhome.com>
2024-11-20 16:47:03 +0300

    add endpoint to fetch desicion tree

--- server/handler/triage.go
+++ server/handler/triage.go
@@ -17,6 +17,7 @@ func InitTriageHandler(router fiber.Router, storage *storage.TriageStorage) {
 	handler := &TriageHandler{storage}
 	router.Get("/", handler.Get)
 	router.Post("/", handler.Post)
+	router.Get("/decision-tree", handler.GetTriageDecisionTree)
 }
 
 func (h *TriageHandler) Get(ctx *fiber.Ctx) error {
@@ -32,6 +33,59 @@ func (h *TriageHandler) Get(ctx *fiber.Ctx) error {
 	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
 		"nodes": nodes,
 		"edges": edges,
+	})
+}
+
+func (h *TriageHandler) GetTriageDecisionTree(ctx *fiber.Ctx) error {
+	context, cancel := context.WithTimeout(ctx.Context(), 5*time.Second)
+	defer cancel()
+
+	tNodes, oNodes, edges, err := h.storage.Get(context)
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	nextStepID := ctx.Query("nextStepId", "")
+	currentNodeStep := &model.TriageNode{}
+	var step string
+	options := []map[string]string{}
+
+	for _, node := range tNodes {
+		if (nextStepID == "" && node.Data.IsRoot) || node.Id == nextStepID {
+			currentNodeStep = node
+			step = node.Data.Value
+			break
+		}
+	}
+
+	for _, ONode := range oNodes {
+		if ONode.ParentId == currentNodeStep.Id {
+			// current node is the parent of the current oNode in the iteration
+			option := make(map[string]string)
+			option["value"] = ONode.Data.Value
+
+			for _, edge := range edges {
+				if edge.Source == ONode.Id {
+					for _, node := range tNodes {
+						if edge.Target == node.Id {
+							if node.Data.StepType == "label" {
+								option["assignedLabel"] = node.Data.AssignedLabel
+							} else {
+								option["nextStep"] = node.Id
+							}
+							break
+						}
+					}
+					break
+				}
+			}
+
+			options = append(options, option)
+		}
+	}
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"step":    step,
+		"options": options,
 	})
 }
 
