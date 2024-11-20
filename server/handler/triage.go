2d74cebf10139aac254e05020e154da7fbc5643f
Daniel Maina <daniel.maina@frontdoorhome.com>
2024-11-20 17:33:00 +0300

    WIP: improve on desicion-tree endpoint performance

--- server/handler/triage.go
+++ server/handler/triage.go
@@ -6,6 +6,7 @@ import (
 	"github.com/danielwangai/hospital_triage/storage"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"log"
 	"time"
 )
 
@@ -18,6 +19,7 @@ func InitTriageHandler(router fiber.Router, storage *storage.TriageStorage) {
 	router.Get("/", handler.Get)
 	router.Post("/", handler.Post)
 	router.Get("/decision-tree", handler.GetTriageDecisionTree)
+	//router.Get("/decision-tree-v2", handler.GetTriageDecisionTreeV2)
 }
 
 func (h *TriageHandler) Get(ctx *fiber.Ctx) error {
@@ -83,6 +85,85 @@ func (h *TriageHandler) GetTriageDecisionTree(ctx *fiber.Ctx) error {
 			options = append(options, option)
 		}
 	}
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"step":    step,
+		"options": options,
+	})
+}
+
+// GetTriageDecisionTreeV2 => TODO: Work on this to improve performance.
+func (h *TriageHandler) GetTriageDecisionTreeV2(ctx *fiber.Ctx) error {
+	context, cancel := context.WithTimeout(ctx.Context(), 5*time.Second)
+	defer cancel()
+
+	tNodes, oNodes, _, err := h.storage.Get(context)
+	if err != nil {
+		return ctx.SendStatus(fiber.StatusBadRequest)
+	}
+
+	nextStepID := ctx.Query("nextStepId", "")
+	currentNodeStep := &model.TriageNode{}
+	var step string
+	options := []map[string]string{}
+
+	for _, node := range tNodes {
+		if (nextStepID == "" && node.Data.IsRoot) || node.Id == nextStepID {
+			currentNodeStep = node
+			step = node.Data.Value
+			break
+		}
+	}
+
+	var childNodes []*model.TriageOptionNode
+
+	for _, ONode := range oNodes {
+		if ONode.ParentId == currentNodeStep.Id {
+			childNodes = append(childNodes, ONode)
+			// current node is the parent of the current oNode in the iteration
+			//option := make(map[string]string)
+			//option["value"] = ONode.Data.Value
+			//
+			//for _, edge := range edges {
+			//	if edge.Source == ONode.Id {
+			//		for _, node := range tNodes {
+			//			if edge.Target == node.Id {
+			//				if node.Data.StepType == "label" {
+			//					option["assignedLabel"] = node.Data.AssignedLabel
+			//				} else {
+			//					option["nextStep"] = node.Id
+			//				}
+			//				break
+			//			}
+			//		}
+			//		break
+			//	}
+			//}
+			//
+			//options = append(options, option)
+		}
+	}
+
+	log.Printf("childNodes>>>: %v\n", childNodes)
+	for _, v := range childNodes {
+		log.Printf("v>>>: %v\n", v)
+	}
+
+	for _, node := range tNodes {
+		for _, child := range childNodes {
+			if child.Id == node.Id {
+				option := make(map[string]string)
+				option["value"] = child.Data.Value
+				if node.Data.StepType == "label" {
+					option["assignedLabel"] = node.Data.AssignedLabel
+				} else {
+					option["nextStep"] = node.Id
+				}
+				options = append(options, option)
+			}
+			log.Printf("child.Id == node.Id>>>: %v\n%s, %s", child.Id == node.Id, child.Id, node.Id)
+		}
+	}
+
 	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
 		"step":    step,
 		"options": options,
